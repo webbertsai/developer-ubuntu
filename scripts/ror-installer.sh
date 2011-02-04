4feebc708b0b75f03d561db03b0bc943c1a60b33
Manic Chuang <manic@pixnet.tw>
2011-02-04 19:23:53 +0800

    add SUDO

--- scripts/ror-installer.sh
+++ scripts/ror-installer.sh
@@ -2,19 +2,22 @@
 
 APTGET="/usr/bin/apt-get"
 DPKG="/usr/bin/dpkg"
+SUDO="/usr/bin/sudo"
 WGET="/usr/bin/wget"
 
 # init, update packages list
-${APTGET} update
-${APTGET} upgrade
+${SUDO} ${APTGET} update
+${SUDO} ${APTGET} upgrade
 
 # install basic tools
-${APTGET} -y install vim git-core tig apache2 mysql-server
+${SUDO} ${APTGET} -y install vim git-core tig apache2 mysql-server
 
 # install ruby tools
-${APTGET} -y install libxml2-dev libxslt-dev libreadline5-dev
+${SUDO} ${APTGET} -y install libxml2-dev libxslt-dev libreadline5-dev
 
 # install REE
 ${WGET} 'http://rubyforge.org/frs/download.php/71098/ruby-enterprise_1.8.7-2010.02_amd64_ubuntu10.04.deb'
-${DPKG} -i ruby-enterprise_1.8.7-2010.02_amd64_ubuntu10.04.deb
+${SUDO} ${DPKG} -i ruby-enterprise_1.8.7-2010.02_amd64_ubuntu10.04.deb
 
+# Add Ruby Enterprise bin to PATH
+echo 'export PATH="/usr/local/lib/ruby/gems/1.8/bin:$PATH"' >> ~/.profile
