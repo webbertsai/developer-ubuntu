b0fc0e2697e62098015169031cd5b21e40c689c1
Manic Chuang <manic.csie90@gmail.com>
2011-06-02 13:43:46 +0800

    use apt-get

--- scripts/flashplayer.sh
+++ scripts/flashplayer.sh
@@ -1,29 +1,8 @@
 #!/bin/sh
 
-ADDGROUP="/usr/sbin/addgroup"
 APTGET="/usr/bin/apt-get"
-CAT="/bin/cat"
-CHMOD="/bin/chmod"
-CHOWN="/bin/chown"
-DPKG="/usr/bin/dpkg"
-ECHO="/bin/echo"
-ENV="/usr/bin/env"
-GROUPADD="/usr/sbin/groupadd"
-HOSTNAME="/bin/hostname"
-KILL="/bin/kill"
-LN="/bin/ln"
-MKDIR="/bin/mkdir"
-MV="/bin/mv"
-PWD="/bin/pwd"
-SED="/bin/sed"
-SHA1SUM="/usr/bin/sha1sum"
-SYSCTL="/sbin/sysctl"
-TAR="/bin/tar"
-USERADD="/usr/sbin/useradd"
-UUDECODE="/usr/bin/uudecode"
-WGET="/usr/bin/wget"
+SUDO="/usr/bin/sudo"
 
-${WGET} 'http://download.macromedia.com/pub/labs/flashplayer10/flashplayer10_2_p3_64bit_linux_111710.tar.gz'
-${TAR} xvfz flashplayer10_2_p3_64bit_linux_111710.tar.gz
-${MKDIR} -p ~/.mozilla/plugins
-${MV} libflashplayer.so ~/.mozilla/plugins
+${SUDO} /usr/bin/add-apt-repository ppa:sevenmachines/flash
+${SUDO} ${APTGET} update
+${SUDO} ${APTGET} install flashplugin64-installer
